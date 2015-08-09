763e53dd1a2bf42c8cccd813da5b5d8f5b8bb756
fremont <jianzhuang900327@163.com>
2015-08-10 04:37:10 +0800

    happy data day for everyday

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data1<-read.table("household_power_consumption.txt",sep=";",na.strings = "?",header = T)
+data2<-subset(data1,data1$Date=="1/2/2007")
+data3<-subset(data1,data1$Date=="2/2/2007")
+datap<-rbind(data2,data3)
+png("plot1.png",width = 480,height = 480)
+hist(datap$Global_active_power,col="red",xlab = "Global Active Power(kilowatts)",main = "Global Active Power",ylim = c(0,1200))
+dev.off()
